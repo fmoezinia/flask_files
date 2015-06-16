4f3040c402d9617c4ae1e3ff15639171e304288c
fmoezinia <moezinia.r@gmail.com>
2015-06-16 14:27:27 -0700

    Number changed
    to rickys

--- test_sms.py
+++ test_sms.py
@@ -7,6 +7,6 @@ client = TwilioRestClient(account_sid, auth_token)
  
 message = client.messages.create(body="This is a message for Monica Moezinia. \
 	We have tried to get in touch with your son, Fredric, regarding his Royal College of Music application. Unfortunately, his UK mobile is unavailable. We would like to inform you that there was an error with his online application last June. Although he was accepted at the RCM, unfortunately, we did not get a receipt of his acceptance, meaning he was not offered a place. We recently updated our online IT support, and noticed the error. Please call the RCM at your own convenience.",
-    to="+447760341444",    # Replace with your phone number
+    to="+17186121018",    # Replace with your phone number
     from_="+16507279462") # Replace with your Twilio number
 print message.sid
