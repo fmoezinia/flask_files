36d84dc03166a247e8b7ea78b6d20bf7d134089a
fmoezinia <moezinia.r@gmail.com>
2015-06-16 14:25:41 -0700

    SMS from twilio account to any number
    first version
    from our twillio zinc number to any US number

---
+++ run.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, redirect
+import twilio.twiml
+ 
+app = Flask(__name__)
+ 
+@app.route("/my_twilio_endpoint", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+ 
+ 	print request.form
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
+  
+@app.route("/test", methods=['GET', 'POST'])
+def test():
+ 	return 'hello world'
+ 
+
+if __name__ == "__main__":
+	#debug=True
+    app.run(host = '0.0.0.0')
---
+++ test_sms.py
@@ -0,0 +1,12 @@
+from twilio.rest import TwilioRestClient
+ 
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "AC26a76547a74db155c8c9fa8a27293047"
+auth_token  = " 6158f22e6e884307f2553706444375ad "
+client = TwilioRestClient(account_sid, auth_token)
+ 
+message = client.messages.create(body="This is a message for Monica Moezinia. \
+	We have tried to get in touch with your son, Fredric, regarding his Royal College of Music application. Unfortunately, his UK mobile is unavailable. We would like to inform you that there was an error with his online application last June. Although he was accepted at the RCM, unfortunately, we did not get a receipt of his acceptance, meaning he was not offered a place. We recently updated our online IT support, and noticed the error. Please call the RCM at your own convenience.",
+    to="+447760341444",    # Replace with your phone number
+    from_="+16507279462") # Replace with your Twilio number
+print message.sid
