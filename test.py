57fdf06b480a2579515391bbdb7f7f045b4f7857
fmoezinia <moezinia.r@gmail.com>
2015-06-16 16:10:10 -0700

    test locally and on server

---
+++ test.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, redirect
+import twilio.twiml
+ 
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def test():
+ 	return 'hello ricky test'
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
+    #app.run(host = '0.0.0.0')
