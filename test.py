31c007be9e358c70a5cf61ec5e29eefd08938f9c
fmoezinia <moezinia.r@gmail.com>
2015-06-16 17:20:08 -0700

    return

--- test.py
+++ test.py
@@ -6,12 +6,13 @@ app = Flask(__name__)
 
 @app.route("/my_twilio_endpoint", methods=['GET', 'POST'])
 def test():
- 	return 'hello ricky test with sms, testing sms then phone'
 	print request.form
 	resp = twilio.twiml.Response()
 	resp.message("Hello, Mobile Monkey. this is a test. if you are seeing this, then the configuration has worked.")
 	#resp.say("Hello, Mobile Monkey. this is a test. if you are hearing this, then the configuration has worked.")
-	return str(resp)
+	#return str(resp)
+ 	return 'hello ricky test with sms, testing sms then phone'
+
 
 if __name__ == "__main__":
     app.run(debug = True, port=80, host = '0.0.0.0')
