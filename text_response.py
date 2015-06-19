c7f7ef9bfdb6c4e778b8293907a65be701c64669
fmoezinia <moezinia.r@gmail.com>
2015-06-19 12:48:41 -0700

    f

---
+++ text_response.py
@@ -0,0 +1,20 @@
+#this script responds to texts
+
+
+from flask import Flask, request, redirect
+import twilio.twiml
+ 
+app = Flask(__name__)
+
+
+@app.route("/my_twilio_endpoint", methods=['GET', 'POST'])
+def test():
+	print request.form
+	resp = twilio.twiml.Response()
+	resp.message("Hello, Mobile Monkey. this is a test. if you are seeing this, then the configuration has worked.")
+	return str(resp)
+	print resp
+
+
+if __name__ == "__main__":
+    app.run(debug = True, port=80, host = '0.0.0.0')
